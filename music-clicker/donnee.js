f845b5c1c122f91b09f02d779e6ace29863a764d
alphadelta12 <143135559+alphadelta12@users.noreply.github.com>
2026-02-23 12:44:15 -0500

    donne test
    #1

--- music-clicker/donnee.js
+++ music-clicker/donnee.js
@@ -1,1 +1,7 @@
- 
+ const lesCartes = [
+    { id: 1, name: "test", rarity: "common", icon: "assets/GinBlossoms.jpg"},
+    { id: 2, name: "testrare", rarity: "rare", icon: "assets/GinBlossoms.jpg"},
+
+    { id: 3, name: "testepic", rarity: "epic", icon: "assets/GinBlossoms.jpg"},
+    { id: 4, name: "testleg", rarity: "legendaire", icon: "assets/GinBlossoms.jpg"},
+ ]
